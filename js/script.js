4fbda5f463d5a0d21c826285c9c72499cb5af26b
OlenaZh <alyona.mesh@gmail.com>
2022-05-02 10:14:49 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Storage.prototype.setObj = function(key, obj) {
 Storage.prototype.getObj = function(key) {
     return JSON.parse(this.getItem(key))
 }
-// CREATE AN ARRAY OF EMPLOYEES
+// CREATE AN ARRAY OF EMPLOYEES (1)
 let arrEmployees = [
     [12345678, "Kate Moss", 1234, "kate@moss.com", "Director"],
     [12345679, "John Moss", 2222, "john@moss.com", "CTO"],
@@ -26,12 +26,12 @@ arrEmployees = localStorage.getObj('employees');
     arrEmployees = JSON.parse(localStorage.getItem('employees'));
 }*/
 
-// GET DOM ELEMENTS
-let form        = document.querySelector('#addForm');
-let empTable    = document.querySelector('#employees');
-let empCount    = document.querySelector('#empCount');
+// GET DOM ELEMENTS (3)
+let form        = document.getElementById('addForm');
+let empTable    = document.getElementById('employees');
+let empCount    = document.getElementById('empCount');
 
-// BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
+// BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS (2)
 buildGrid(arrEmployees);
 
 // ADD EMPLOYEE
@@ -40,11 +40,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // GET THE VALUES FROM THE TEXT BOXES
-    let empID    = parseInt(document.querySelector('#id').value);
-    let empName  = document.querySelector('#name').value;
-    let empExt   = parseInt(document.querySelector('#extension').value);
-    let empEmail = document.querySelector('#email').value;
-    let empDept  = document.querySelector('#department').value;
+    let empID    = parseInt(document.getElementById('id').value);
+    let empName  = document.getElementById('name').value;
+    let empExt   = parseInt(document.getElementById('extension').value);
+    let empEmail = document.getElementById('email').value;
+    let empDept  = document.getElementById('department').value;
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
     let arrNewEmployee = [empID, empName, empExt, empEmail, empDept];
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
@@ -52,9 +52,9 @@ form.addEventListener('submit', (e) => {
     // BUILD THE GRID
     buildGrid(arrEmployees);
     // RESET THE FORM
-    document.querySelector('#addForm').reset();
+    document.getElementById('addForm').reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-    document.querySelector('#id').focus();
+    document.getElementById('id').focus();
 
 });
 
@@ -78,12 +78,12 @@ empTable.addEventListener('click', (e) => {
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid(arrEmployees) {
-    // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
+    // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION (4)
     empTable.lastElementChild.remove();
-    // REBUILD THE TBODY FROM SCRATCH
+    // REBUILD THE TBODY FROM SCRATCH (5)
     let tbody = document.createElement('tbody');
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
-    // REBUILDING THE ROW STRUCTURE
+    // REBUILDING THE ROW STRUCTURE (6) Build new table
     for (let employee of arrEmployees) {
         tbody.innerHTML +=
         `
@@ -97,7 +97,7 @@ function buildGrid(arrEmployees) {
         </tr>
         `
     }
-    // BIND THE TBODY TO THE EMPLOYEE TABLE
+    // BIND THE TBODY TO THE EMPLOYEE TABLE (7)
     empTable.appendChild(tbody);
 
     // UPDATE EMPLOYEE COUNT
